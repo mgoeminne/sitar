579f9777585c10f994d57b34aab71701eca97279
mgoeminne <mgoeminne@gmail.com>
2015-12-04 15:05:31 +0100

    Lets the inference engine decide the type of manipulated objects instead of force it.

--- src/main/scala/com/github/mgoeminne/sitar/parser/StyleParser.scala
+++ src/main/scala/com/github/mgoeminne/sitar/parser/StyleParser.scala
@@ -7,30 +7,58 @@ import com.github.mgoeminne.sitar.parser.acm.{ACMProceedingsParser, ACMBookParse
   */
 trait StyleParser
 {
+   /**
+     * @return a citation parser for inproceedings citations
+     */
    def inProceedingsParser    : CitationParser
+
+   /**
+     * @return a citation parser for article citations
+     */
    def articleParser          : CitationParser
+
+   /**
+     * @return a citation parser for book chapter citations
+     */
    def bookChapterParser      : CitationParser
+
+   /**
+     * @return a citation parser for technical report citations
+     */
    def technicalReportParser  : CitationParser
 
+   /**
+     * @return a citation parser for book citations
+     */
    def bookParser             : CitationParser
+
+   /**
+     * @return a citation parser for proceedinds citations
+     */
    def proceedindsParser      : CitationParser
 
+   /**
+     * @return a citation parser for thesis citations
+     */
    def thesisParser           : CitationParser
 
+   /**
+     * @return a sequence of all parsers
+     */
    private def parsers: Seq[CitationParser] = Seq( inProceedingsParser, articleParser,
                                                    bookChapterParser, technicalReportParser,
                                                    bookParser, proceedindsParser)
 
    /**
      * Tries to parse a citation using a parser for a particular style of citation.
-     * @param citation  The potential citation string
-     * @return          The extracted citation, if any
+     * @param line      The potential citation string.
+     * @return          The extracted citation, if any.
      */
-   def parse(citation: String): Option[Citation] =
+   def parse(line: String): Option[Citation] =
    {
-      parsers.foldLeft(None.asInstanceOf[Option[Citation]])((previous, p) => previous match {
+      parsers.foldLeft(Option.empty[Citation])((previous, p) => previous match {
          case s: Some[Citation] => s
-         case _ => p.parseAll(p.citation, citation) match {
+         case _ => p.parseAll(p.citation, line) match {
             case p.Success(result: Citation, _) => Some(result)
             case _ => None
          }
